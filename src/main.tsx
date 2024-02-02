dff392c470de3f4cd975ca486d9c82600d47c5c2
KaiYun Cheng <tanyacheng@gmail.com>
2024-02-01 22:23:16 -0500

    fix: changed to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from '@/App.tsx';
 import '@/assets/styles/index.css';
@@ -9,9 +9,9 @@ import store from '@/store/store.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
